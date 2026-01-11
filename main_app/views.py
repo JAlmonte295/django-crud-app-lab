92de9525920d20e6d7bd04180179623f49e911ff
jalmontegit <jalmonte721@gmail.com>
2026-01-11 18:36:06 -0500

    rendering data in templates

--- main_app/views.py
+++ main_app/views.py
@@ -1,14 +1,29 @@
 from django.shortcuts import render
 
-# Import HttpResponse to send text-based responses
-from django.http import HttpResponse
-
 # Define the home view function
 def home(request):
     # Send a simple HTML response
-    return render(request, 'base.html')
+    return render(request, 'home.html')
 
 # Define the about view function
 def about(request):
     # Send about page response
     return render(request, 'about.html')
+
+class Item:
+    def __init__(self, name, type, price, description):
+        self.name = name
+        self.type = type
+        self.price = price
+        self.description = description
+
+items = [
+    Item('Max Steel', 'Toy', 100.00, 'bough when i was a child'),
+    Item('Game Boy', 'Video Game', 0.00, 'passed down my my parents'),
+    Item('Pokemon Gen 1 Pack', 'Cards', 10.00, 'unopened, bought back in the early 2000s'),
+]
+
+def item_index(request):
+    return render(request, 'items/index.html', {'items': items})
+
+# Create your views here.
