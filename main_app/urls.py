96fc1ace7ade22f8544f8fa2d6f5352994a42512
jalmontegit <jalmonte721@gmail.com>
2026-01-11 21:04:20 -0500

    CRUD implemented

--- main_app/urls.py
+++ main_app/urls.py
@@ -5,4 +5,8 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('about/', views.about, name='about'),
     path('items/', views.item_index, name='item-index'),
+    path('items/<int:item_id>/', views.item_detail, name='item-detail'),
+    path('items/create/', views.ItemCreate.as_view(), name='item-create'),
+    path('items/<int:pk>/update/', views.ItemUpdate.as_view(), name='item-update'),
+    path('items/<int:pk>/delete/', views.ItemDelete.as_view(), name='item-delete'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Item
 
 # Define the home view function
 def home(request):
@@ -10,20 +12,26 @@ def about(request):
     # Send about page response
     return render(request, 'about.html')
 
-class Item:
-    def __init__(self, name, type, price, description):
-        self.name = name
-        self.type = type
-        self.price = price
-        self.description = description
-
-items = [
-    Item('Max Steel', 'Toy', 100.00, 'bough when i was a child'),
-    Item('Game Boy', 'Video Game', 0.00, 'passed down my my parents'),
-    Item('Pokemon Gen 1 Pack', 'Cards', 10.00, 'unopened, bought back in the early 2000s'),
-]
-
 def item_index(request):
+    items = Item.objects.all()
     return render(request, 'items/index.html', {'items': items})
 
+def item_detail(request, item_id):
+    item = Item.objects.get(id=item_id)
+    return render(request, 'items/detail.html', {'item': item})
+
+
 # Create your views here.
+
+class ItemCreate(CreateView):
+    model = Item
+    fields = '__all__'
+
+class ItemUpdate(UpdateView):
+    model = Item
+    fields = ['name', 'item_type', 'price', 'description']
+
+class ItemDelete(DeleteView):
+    model = Item
+    success_url = '/items/'
+
